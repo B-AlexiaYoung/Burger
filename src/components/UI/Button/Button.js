5e04be0e4d61918051e0f6e7629cddcd3032b8af
B-AlexiaYoung <brlxyng@gmail.com>
2018-12-01 11:39:06 -0700

    working sidedrawer implemented

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import classes from './Button.css';
 
-const Button = (props)=> (
+const button = (props) => (
     <button
-    className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.clicked}>{props.children}</button>
-)
-export default Button
+        className={[classes.Button, classes[props.btnType]].join(' ')}
+        onClick={props.clicked}>{props.children}</button>
+);
+
+export default button;
