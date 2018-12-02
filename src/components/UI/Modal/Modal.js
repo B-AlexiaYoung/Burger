dbed862587fc08853e052aca9099919535cc034f
B-AlexiaYoung <brlxyng@gmail.com>
2018-12-02 15:25:10 -0700

    spinner added

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,15 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxillary/Auxillary';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
-    shouldComponentUpdate(nextProps, nextState){
-      return  nextProps !== this.props.show        
+
+    shouldComponentUpdate ( nextProps, nextState ) {
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
-    render(){
-        return(
+
+    componentWillUpdate () {
+        console.log('[Modal] WillUpdate');
+    }
+
+    render () {
+        return (
             <Aux>
                 <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
                 <div
@@ -21,9 +27,8 @@ class Modal extends Component {
                     {this.props.children}
                 </div>
             </Aux>
-
         )
     }
-} 
+}
 
 export default Modal;
